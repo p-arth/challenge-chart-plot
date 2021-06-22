1c71a35fafc66b1a26588a10580423d2a78e4d07
Pedro Arthur <pedro.veloso@spot-edu.com.br>
2021-06-22 13:01:08 -0300

    Created UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import UserInput from './components/UserInput';
 
 function App() {
   return (
     <div className="main-container">
       <Header />
+      <UserInput />
 
       <Footer />
     </div>
